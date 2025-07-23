fcec9ce64765ee298551babd9224ee9e080de305
ehdgn <ehdgn6570@gmail.com>
2025-07-23 17:45:09 +0900

    20250723_2

---
+++ src/main/java/org/example/chapter15/c_board_app/dto/BoardResponseDto.java
@@ -0,0 +1,23 @@
+package org.example.chapter15.c_board_app.dto;
+
+// BoardResponseDto
+// : DB 게시판 데이터 중 사용자에게 응답으로 전달할 객체 구조
+// - content 값을 제외한 필드를 제공
+
+import org.example.chapter15.c_board_app.entity.Board;
+
+public class BoardResponseDto {
+    private long id;
+    private String title;
+    private String author;
+
+    public BoardResponseDto(Board board) {
+        this.id = board.getId();
+        this.title = board.getTitle();
+        this.author = board.getAuthor();
+    }
+
+    public static BoardResponseDto fromEntity(Board board) {
+        return new BoardResponseDto(board);
+    }
+}
